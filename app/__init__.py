bd7e4faed9928e74e5277941158aff5b7607c9d1
Lucas Lazaroiu <73948966+luksy26@users.noreply.github.com>
2024-04-04 19:40:46 +0300

    Created results directory

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,14 @@ Flask app for data analysis tasks.
 * Manages a thread pool.
 * Reads data from "nutrition_activity_obesity_usa_subset.csv".
 * Stores processed data in `DataIngestor.questions_dict`.
-* Tracks jobs with `webserver.job_counter` (commented out).
+* Tracks jobs with `webserver.job_counter`.
+* Creates the 'results' directory that is used by taskRunners to store
+the result data for a certain job_id
 """
 
 import pprint
+import os
+import shutil
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
@@ -26,3 +30,22 @@ webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.
 webserver.job_counter = 1
 
 from app import routes
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Define the desired path for the "results" directory
+results_dir = os.path.join(cwd, "results")
+
+# Check if the directory exists
+if os.path.exists(results_dir):
+    # Delete the existing directory and its contents (be cautious!)
+    shutil.rmtree(results_dir)
+    print("Existing directory 'results' deleted.")
+    # Recreate the empty directory
+    os.makedirs(results_dir)
+    print("Directory 'results' created successfully!")
+else:
+    # Create the directory if it doesn't exist
+    os.makedirs(results_dir)
+    print("Directory 'results' created successfully!")
